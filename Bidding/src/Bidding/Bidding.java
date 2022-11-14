4e5046c4d1c0f2be2d3042afab63d5b65f059621
cccdb2022 <98005783+cccdb2022@users.noreply.github.com>
2022-11-14 15:46:54 -0600

    Add files via upload

---
+++ Bidding/src/Bidding/Bidding.java
@@ -0,0 +1,36 @@
+package Bidding;
+
+import java.util.List;
+
+public class Bidding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public static boolean ListForAuction(int token, int user_id, int starting_price, 
+			int buy_now_price, int start_time, int expire_time) {
+		return true;
+	}
+	
+	public static boolean PlaceBid(int token, int user_id, int auction_id, int bid_price, int buynow) {
+		return true;
+	}
+	
+	public static List<Auction> GetAuction(String category, String keyword, String sort_key, int order){
+		return null;
+	}
+	
+	public static boolean UpdateAuction(String data) {
+		return true;
+	}
+	
+	public class Auction{
+		int auction_id;
+		public Auction(int auction_id) {
+			this.auction_id = auction_id;
+		}
+	}
+}
+
